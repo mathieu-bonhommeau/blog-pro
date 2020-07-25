bfaaa7bf531edd4e3c29c3b4975c1b30514a991f
matanna <mathieu.bonhommeau@orange.fr>
2020-07-25 19:12:07 +0200

    create template with twig

--- index.php
+++ index.php
@@ -1,38 +1,29 @@
 <?php
+//Routing
+require 'vendor/autoload.php';
 
-require __DIR__.'/vendor/autoload.php';
 
-require 'model/entity/Post.php';
-require 'model/manager/Manager.php';
-require 'model/manager/PostManager.php';
-require 'model/manager/CommentManager.php';
-require 'model/entity/Comment.php';
-require 'model/entity/User.php';
-require 'model/manager/UserManager.php';
-try 
+//Rendu template
+$loader = new Twig\Loader\FilesystemLoader('view');
+$twig = new Twig\Environment(
+    $loader, [
+    'cache' => false //'/tmp'
+    ]
+);
+
+
+if (isset($_GET['p']) && $_GET['p']=='home')
 {
-    $postManager = new PostManager;
-    $data = $postManager->addPost('bla', 'bla', 'bla', 1);
-    $data = $postManager -> getPost(14);
-    $post = new Post($data);
-    dump($post);
-    echo $post->lastDateModif();
-    $commentManager = new CommentManager;
-    $datas = $commentManager -> getComment(2);
-    dump($datas);
-    $comment = new Comment($datas);
-    dump($comment);
-    $userManager = new UserManager;
-    $data = $userManager->getUser(5);
-    $user = new User($data);
-    dump($data);
-    dump($user);
-    
-
-    
-
+    echo $twig->render('frontView/homeView.twig');
 }
-catch(Exception $e)
+elseif (isset($_GET['p']) && $_GET['p']=='listposts')
 {
-    die('Erreur:' . $e->getCode() . $e->getMessage());
+    echo $twig -> render('frontView/listPostView.twig');
 }
+else {
+    echo $twig->render('template.twig');
+}
+
+?>
+
+
